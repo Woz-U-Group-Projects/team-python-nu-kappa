8ac5e60f9fb2705cdd4e8331746bfb4d60db0549
Kayisha Pinkard <kpinkard46@gmail.com>
2020-11-19 23:25:33 -0600

    Started rerouting

--- server-express-mongodb/app.js
+++ server-express-mongodb/app.js
@@ -20,7 +20,7 @@ app.use("/tasks", tasksRouter);
 
 //var mongoDB = "mongodb://127.0.0.1/database";
 var mongoDB =
-  "mongodb+srv://ammon:Password1%21@cluster0-lhvh5.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://FoodTruck:Password1!@cluster0.rmbs9.mongodb.net/test";
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
