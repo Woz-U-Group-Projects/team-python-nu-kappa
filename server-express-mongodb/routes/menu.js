5f4e9a95424b3b278b82a3629483266d05cf8d0b
Maria Arfeen <mariaarfeen1@gmail.com>
2020-11-25 20:03:11 -0500

    updated name of tasks.js to menu.js in routes

--- server-express-mongodb/routes/menu.js
+++ server-express-mongodb/routes/menu.js
@@ -1,10 +1,6 @@
 var express = require("express");
 var router = express.Router();
-<<<<<<< HEAD:server-express-mongodb/routes/tasks.js
-var TaskModel = require("../models/menu");
-=======
 var MenuModel = require("../models/menu");
->>>>>>> 8694d1b1b979139a06b597515b2140f534661037:server-express-mongodb/routes/menu.js
 
 router.get("/", function(req, res, next) {
   MenuModel.find().then(menu => res.json(menu));
