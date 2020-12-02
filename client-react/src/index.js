51ea85e71a1869b75ea1eb142be63d8a7a71209b
Charlene Macias <charlenemacias94@gmail.com>
2020-12-01 21:42:25 -0800

    added props and menu.js to routes

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import PropTypes from 'prop-types';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
