05ffec682d39c5ae1bac5b620b23b1d7f9f0ed86
Charlene Macias <charlenemacias94@gmail.com>
2020-12-03 22:47:37 -0800

    Created basic pages and navigation

---
+++ client-react/src/components/FoodTruckInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FoodTruckInfo = () => {
+    return(
+        <div>
+            <h1>Food Truck Info</h1>
+            <p>Food Truck map picture, contact info, hours, etc</p>
+        </div>
+    );
+}
+
+export default FoodTruckInfo;
