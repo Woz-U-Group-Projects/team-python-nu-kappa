5012787b23b274c89cc697ed570bc7ec5c015c8b
Maria Arfeen <mariaarfeen1@gmail.com>
2020-12-18 17:08:36 -0500

    added pictures to the menu page

--- client-react/src/components/FoodTruckMenu.js
+++ client-react/src/components/FoodTruckMenu.js
@@ -62,31 +62,31 @@ class FoodTruckMenu extends React.Component {
         <body>
          <h1>Entrees</h1>  
          <ul>
-           <li>Vegan Pizza: Gluten free, organic tomatoes, spinach, roasted cauliflower, olives and red onions. $12.00 <img src="VeganPizza.jpg"></img> </li>
-           <li>Broiled Salmon with a Chilli-Mango Sauce: Pungent red chiles and sweet mango flavor this robust sauce, which accents simply broiled salmon wonderfully. $10.00< img src="salmon.jpg"></img></li>
-           <li>Taco Salad: It’s a Taco with no shell! $13.00 <img src="tacosalad.jpg"></img></li>          
-           <li>Naked Chix Bowl: Mango basil chicken with Hawaiian mix on top of jasmine rice with cilantro mayo. $9.00 <img src="chickenbowl.jpg"></img></li>          
+           <li>Vegan Pizza: Gluten free, organic tomatoes, spinach, roasted cauliflower, olives and red onions. $12.00 <br></br> <img src="VeganPizza.jpg" width="300" height="300"></img> </li>
+           <li>Broiled Salmon with a Chilli-Mango Sauce: Pungent red chiles and sweet mango flavor this robust sauce, which accents simply broiled salmon wonderfully. $10.00 <br></br> <img src="salmon.jpg" width="300" height="300"></img></li>
+           <li>Taco Salad: It’s a Taco with no shell! $13.00 <br></br> <img src="tacosalad.jpg" width="300" height="300"></img></li>          
+           <li>Naked Chix Bowl: Mango basil chicken with Hawaiian mix on top of jasmine rice with cilantro mayo. $9.00 <br></br> <img src="chickenbowl.jpg" width="300" height="300"></img></li>          
          </ul>
          <h1>Sides</h1>
          <ul>
-         <li>Brown Rice Basil: Lemon juice, pink Himalayan salt, pepper, roasted garlic, tomatoes, thyme and boiled in coconut water. $3.00 <img src="basilbrownrice.png"></img></li>
-         <li>Wild Rice Pilaf: This healthy rice pilaf combines wild rice with brown rice for a nutty, delicious side dish. $2.00 <img src="wildricepilaf.jpg"></img></li>
-         <li>Orange-Glazed Carrots: It’s Vitamin Rich Carrots! $4.00 <img src="carrots.jpg"></img></li>
-        <li>Caulifire: A sautéed cauliflower buffalo ‘chicken’ with ranch. $2.00 <img src="caulifire.jpg"></img></li>
+         <li>Brown Rice Basil: Lemon juice, pink Himalayan salt, pepper, roasted garlic, tomatoes, thyme and boiled in coconut water. $3.00 <br></br> <img src="basilbrownrice.png" width="300" height="250"></img></li>
+         <li>Wild Rice Pilaf: This healthy rice pilaf combines wild rice with brown rice for a nutty, delicious side dish. $2.00 <br></br> <img src="wildricepilaf.jpg" width="300" height="300"></img></li>
+         <li>Orange-Glazed Carrots: It’s Vitamin Rich Carrots! $4.00 <br></br> <img src="carrots.jpg" width="300" height="300"></img></li>
+        <li>Caulifire: A sautéed cauliflower buffalo ‘chicken’ with ranch. $2.00 <br></br> <img src="caulifire.jpg" width="300" height="300"></img></li>
          </ul>
          <h1>Drinks</h1>
          <ul>
-         <li>Cucumber Lemonade: Lemonade made from blended cucumbers, fresh lemon juice and honey. $2.00 <img src="lemonade.jpg"></img></li>
-        <li>Peach Tea Spritzer: Peaches, lemon juice, chamomile tea, honey and mint leaves. $4.00</li>
-        <li>Ginger Tea: It’s Tea with Ginger in it! $3.00</li>
-        <li>S’mores Cold Brew: A cold brew coffee for anyone that loves s’mores. $4.00</li>
+         <li>Cucumber Lemonade: Lemonade made from blended cucumbers, fresh lemon juice and honey. $2.00 <br></br> <img src="lemonade.jpg" width="300" height="300"></img></li>
+        <li>Peach Tea Spritzer: Peaches, lemon juice, chamomile tea, honey and mint leaves. $4.00 <br></br> <img src="peachtea.jpg" width="300" height="300"></img></li>
+        <li>Ginger Tea: It’s Tea with Ginger in it! $3.00 <br></br> <img src="gingertea.jpg" width="300" height="300"></img></li>
+        <li>S’mores Cold Brew: A cold brew coffee for anyone that loves s’mores. $4.00 <br></br> <img src="smorescoldbrew.jpg" width="300" height="300"></img></li>
          </ul>
          <h1>Desserts</h1>
          <ul>
-         <li>Bananna Split: Vegan ice cream, cashew milk, coconut milk, organic almonds and cherries. $7.00</li>
-         <li>Lemon Raspberry Frozen Yogurt: A healthy, sweet, refreshing treat for your tummy. $4.00</li>
-         <li>Red Velvet Fudge: It’s made with Beets! $7.00</li>
-         <li>Peaches and Cream Protein Muffins: A light and delicious muffin made with Greek yogurt and diced peaches. $2.00</li>
+         <li>Banana Split: Vegan ice cream, cashew milk, coconut milk, organic almonds and cherries. $7.00 <br></br> <img src="bananasplit.jpeg" width="350" height="200"></img></li>
+         <li>Lemon Raspberry Frozen Yogurt: A healthy, sweet, refreshing treat for your tummy. $4.00 <br></br> <img src="lemonraspberry.jpg" width="300" height="300"></img></li>
+         <li>Red Velvet Fudge: It’s made with Beets! $3.00 <br></br> <img src="redvelvet.jpg" width="300" height="250"></img></li>
+         <li>Peaches and Cream Protein Muffins: A light and delicious muffin made with Greek yogurt and diced peaches. $2.00 <br></br> <img src="muffin.jpg" width="250" height="300"></img></li>
          </ul>
         </body>
     </div>
